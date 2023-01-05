6bb28c5492b0a61fb5f449754e554aaa3aef76f2
Karine Antoniacomi <karineas@pbtech.net.br>
2023-01-05 00:18:30 -0300

    Adiciona rotas de navegação e View Model compartilhado

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -15,9 +15,26 @@
  */
 package com.example.cupcake
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 
 /**
  * Activity for cupcake order flow.
  */
-class MainActivity : AppCompatActivity(R.layout.activity_main)
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        // acessa instancia do NavController no NavHostFragment
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        // mostra um título na barra de apps com base no rótulo do destino e exibe
+        // o botão Para cima sempre que não estiver em um destino de nível superior
+        setupActionBarWithNavController(navController)
+    }
+}
